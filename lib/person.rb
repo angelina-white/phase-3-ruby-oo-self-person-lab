946a3f76b03bceb42c2fe98e971f469d79262df3
Angelina <angelinarwhite@gmail.com>
2022-06-11 14:40:41 -0400

    commit tree 09bb981aa404bd2b86cb1ae8b213c623e24234fd

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,63 @@
 # your code goes here
+require 'pry'
+
+class Person
+
+    attr_reader :name, :happiness, :hygiene
+    attr_writer :happiness, :hygiene
+    attr_accessor :bank_account
+
+    def initialize(name, bank_account=25, happiness=8, hygiene=8)
+        @name=name
+        @bank_account=bank_account
+        @happiness=happiness
+        @hygiene=hygiene
+    end    
+
+    def happiness=(value)
+        if value < 11 && value >= 0
+            @happiness = value
+        else
+            @happiness
+        end
+    end
+
+    def happiness
+        self.happiness
+    end
+
+    def hygiene=(value)
+        if value < 11 && value >= 0
+            @hygiene = value
+        else
+            @hygiene
+        end
+    end
+
+    def hygiene
+        @hygiene
+    end
+
+    def happy
+        if @happiness > 7
+            true
+        else
+            false
+        end
+    end
+
+    def clean
+        if @hygiene > 7
+            true
+        else
+            false
+        end
+    end
+
+    def get_paid=(value)
+        @bank_account= @bank_account + value
+        puts "all about the benjamins"
+    end
+
+    binding.pry
+end
